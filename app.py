bc2d09fcd7b5dc743a481149f3b30904efd9f75f
Drevlyanin <121987689+Drevlyanin@users.noreply.github.com>
2023-04-26 23:30:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
-
 import facebook 
 import requests
-
 
 app_id = 'YOUR_APP_ID'
 app_secret = 'YOUR_APP_SECRET'
 access_token = app_id + '|' + app_secret
 
 graph = facebook.GraphAPI(access_token=access_token, version="12.0")
-graph.put_photo(image=open('image.jpg', 'rb'), message='text')
 
 try:
     post_id = graph.put_photo(image=open('image.jpg', 'rb'), message='text')
     if post_id:
-        print('error')
+        print('Photo posted successfully on Facebook.')
 except Exception as e:
     print(f"Error: {e}")
